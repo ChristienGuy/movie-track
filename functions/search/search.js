0323d0a2f78be54d18cb3f86f19f4072768ef79e
Chris Guy <christien.guy@gmail.com>
2019-05-11 18:10:04 +0100

    Initial search and movie details netlify functions work

---
+++ functions/search/search.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+const fetch = require("node-fetch");
+const { URL, URLSearchParams } = require("url");
+
+const { TMDB_API_KEY } = process.env;
+
+exports.handler = async function(event, context) {
+  const url = new URL(`https://api.themoviedb.org/3/search/multi/`);
+  url.search = new URLSearchParams({
+    api_key: TMDB_API_KEY,
+    query: event.queryStringParameters.query
+  });
+
+  try {
+    const response = await fetch(url, {
+      headers: { Accept: "application/json" }
+    });
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ statusText: response.statusText })
+      };
+    }
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+async function getMovie(movieId: number) {
+  return await fetch(`/.netlify/functions/movie/${movieId}`)
+    .then(res => res.json())
+    .then(json => json);
+}
+
+async function search(query: string) {
+  return await fetch(`/.netlify/functions/search/?query=${query}`)
+    .then(res => res.json())
+    .then(json => json);
+}
 
 const App: React.FC = () => {
+  const [results, setResults] = useState<{
+    results: {
+      title: string;
+      media_type: string;
+      overview: string;
+    }[];
+  }>({
+    results: []
+  });
+
+  const [query, setQuery] = useState("");
+
+  // useEffect(() => {
+  //   getMovie(533).then(movieData => {
+  //     setMovieData(movieData);
+  //   });
+  // }, []);
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    search(query).then(results => {
+      setResults(results);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input onChange={e => setQuery(e.target.value)} value={query} />
+        <input type="submit" value="Search" />
+      </form>
+
+      <ul>
+        {results.results.map(({ title, media_type, overview, ...rest }) => (
+          <li>
+            <div>{title}</div>
+            <div>{media_type}</div>
+            <div>{overview}</div>
+            <pre>{JSON.stringify(rest, null, 2)}</pre>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
